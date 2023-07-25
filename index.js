a88ea2cafbd0d0d1da12e702ce1a68db150d9821
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-07-25 17:45:00 +0300

    Homework 1

---
+++ index.js
@@ -0,0 +1,52 @@
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const contacts = require('./contacts');
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case 'get':
+      const contactById = await contacts.getContactById(id);
+      console.log(contactById);
+      break;
+
+    case 'add':
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+
+    case 'remove':
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    case 'update':
+      const updateContact = await contacts.updateContact(id, {
+        name,
+        email,
+        phone,
+      });
+      console.log(updateContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
